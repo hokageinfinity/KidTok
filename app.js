116fad0805103d699584f9cb0c8e868bc4583396
hokageinfinity <nateandjess1218@gmail.com>
2026-02-08 22:21:48 -0500

    Update video URLs and add autoplay functionality

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const approvedVideos = [
   // Ms Rachel (official)
-  https://www.youtube.com/embed/qXKsou9UmfY?rel=0
+  "https://www.youtube.com/embed/qXKsou9UmfY?controls=0&rel=0&modestbranding=1",
 
   // Blippi (official)
-  "https://www.youtube.com/embed/5p8lP5x7Zq8?controls=0",
+  "https://www.youtube.com/embed/5p8lP5x7Zq8?controls=0&rel=0&modestbranding=1",
 
-  // Bluey clips (official channel only)
-  "https://www.youtube.com/embed/7l3vS8T5YIQ?controls=0",
+  // Bluey (official clips)
+  "https://www.youtube.com/embed/7l3vS8T5YIQ?controls=0&rel=0&modestbranding=1",
 
   // Super Why
-  "https://www.youtube.com/embed/K4K9cF8mT8M?controls=0"
+  "https://www.youtube.com/embed/K4K9cF8mT8M?controls=0&rel=0&modestbranding=1"
 ];
 
 const feed = document.getElementById("feed");
@@ -17,12 +17,26 @@ const feed = document.getElementById("feed");
 approvedVideos.forEach(url => {
   const div = document.createElement("div");
   div.className = "video";
+
   div.innerHTML = `
-<iframe 
-  src="${url}&playsinline=1"
-  allow="autoplay; encrypted-media"
-  playsinline
-></iframe>`;
+    <div class="tap-overlay">▶ Tap to Play</div>
+    <iframe
+      src="${url}&playsinline=1"
+      allow="autoplay; encrypted-media"
+      playsinline
+    ></iframe>
+  `;
+
+  const overlay = div.querySelector(".tap-overlay");
+  const iframe = div.querySelector("iframe");
+
+  overlay.addEventListener("click", () => {
+    overlay.style.display = "none";
+    iframe.src += "&autoplay=1";
+  });
+
   feed.appendChild(div);
 });
+
+// Disable long-press menus (kid safety)
 document.addEventListener("contextmenu", e => e.preventDefault());
