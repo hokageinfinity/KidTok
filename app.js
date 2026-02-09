be4202b576340c7fc35fe9cc4d8b39f672226945
hokageinfinity <nateandjess1218@gmail.com>
2026-02-08 21:47:55 -0500

    Add approved videos to the feed

---
+++ app.js
@@ -0,0 +1,22 @@
+const approvedVideos = [
+  // Ms Rachel (official)
+  "https://www.youtube.com/embed/6Zh8nPzZP6o?controls=0",
+
+  // Blippi (official)
+  "https://www.youtube.com/embed/5p8lP5x7Zq8?controls=0",
+
+  // Bluey clips (official channel only)
+  "https://www.youtube.com/embed/7l3vS8T5YIQ?controls=0",
+
+  // Super Why
+  "https://www.youtube.com/embed/K4K9cF8mT8M?controls=0"
+];
+
+const feed = document.getElementById("feed");
+
+approvedVideos.forEach(url => {
+  const div = document.createElement("div");
+  div.className = "video";
+  div.innerHTML = `<iframe src="${url}" allowfullscreen></iframe>`;
+  feed.appendChild(div);
+});
